418a5ba5a0d6377a30a3955ab3d6638a9ad90840
Humberto Tejada <humtej1204@gmail.com>
2023-07-18 11:42:31 -0500

    [Update]: Add script to install LazyGit

--- shell_scripts/mainScript.sh
+++ shell_scripts/mainScript.sh
@@ -20,6 +20,12 @@ pip install awsebcli --upgrade --user
 sudo apt install git
 git config --global user.name "Humberto Tejada"
 git config --global user.email "humtej1204@gmail.com"
+## LAZYGIT
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -rf lazygit lazygit.tar.gz
 ## NVM (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.bashrc
@@ -33,6 +39,14 @@ npm install -g npm@latest
 sudo npm install -g @angular/cli
 ## REACT JS
 sudo npm -g install create-react-app
+## MONGODB
+sudo apt-get install mongodb -y
+mkdir -p /data/db
+sudo chown -R `id -un` /data/db
+##### para iniciar mongodb primero debemos verificar el nombre del servicio
+##### ya que en algunos lados puede tener un nombre diferente como "mongodb, mongodb-server, mongodb-org, etc".
+##### esto se hace con: sudo service --status-all
+##### una vez encontrado el nombre del servicio mongo
 ## JAVA
 sudo apt install openjdk-8-jre-headless -y
 sudo apt install openjdk-11-jre-headless -y
